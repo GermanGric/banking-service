e36b0e609df252f92458544945043be25a16d025
GermanGric <gric427@gmail.com>
2024-05-29 18:45:11 +0300

    -_-

--- src/main/java/com/example/bankingservice/model/BankAccount.java
+++ src/main/java/com/example/bankingservice/model/BankAccount.java
@@ -11,7 +11,7 @@ public class BankAccount {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
     @Min(0)
     private double balance;
--- src/main/java/com/example/bankingservice/model/User.java
+++ src/main/java/com/example/bankingservice/model/User.java
@@ -14,7 +14,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
     @NotNull
     @Column(unique = true)
